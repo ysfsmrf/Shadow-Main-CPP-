b36e7ab6ad46398d495bd7c5354883d73ab4fdf8
ysfsmrf <wer0011yq@gmail.com>
2026-07-01 17:26:43 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
-#include
+#include "player_stat_general.json"
+
+if
+  player_health=0>:
+  player_is_living()
+else if
+  player_health=0<:
+  player_is_not_living()
+else
+  situation_is_neither_of_the_two()
+
